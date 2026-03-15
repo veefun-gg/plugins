ceab40e7901a41ef81584984a1d729a9956dc914
Anders Rydholm <anders@primetimeweb.com>
2026-03-15 16:01:53 -0500

    Fix Pokedex single-page layout and local Playwright workflow

--- pokedex/templates/single-pokedex.php
+++ pokedex/templates/single-pokedex.php
@@ -786,24 +786,6 @@ $pokeRand = rand(1,898);
 
 
 
-<div id="primary" class="content-area">
-
-	<main id="main" class="site-main" role="main">
-
-		<?php 
-
-			if ( have_posts() ) { get_template_part( 'loop' ); } 
-
-			else { get_template_part( 'content', 'none' ); }
-
-		?>
-
-	</main><!-- #main -->
-
-</div><!-- #primary -->
-
-
-
 <?php
 
 
