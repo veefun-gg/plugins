b6f7410f89869f6ef1d260b00fa43d3f3778532a
Anders Rydholm <anders@primetimeweb.com>
2026-07-25 11:59:58 -0500

    Harden Price Guide card metadata rendering

--- pokemon-price-guide/functions/06-page-template.php
+++ pokemon-price-guide/functions/06-page-template.php
@@ -2,18 +2,57 @@
 /* META Filter
 ---------------------------------------------------------------------------------------*/
 
+function ptp_priceguide_cached_card_data( $cache ) {
+    if ( ! $cache || empty( $cache->cached_meta ) ) {
+        return false;
+    }
+
+    $data = maybe_unserialize( $cache->cached_meta );
+
+    return is_array( $data ) ? $data : false;
+}
+
+function ptp_priceguide_array_field( $data, $key ) {
+    return ( isset( $data[$key] ) && is_array( $data[$key] ) ) ? $data[$key] : array();
+}
+
+function ptp_priceguide_scalar_field( $data, $key, $fallback = 'N/A' ) {
+    if ( isset( $data[$key] ) && ! is_array( $data[$key] ) && $data[$key] !== '' ) {
+        return $data[$key];
+    }
+
+    return $fallback;
+}
+
+function ptp_priceguide_nested_scalar_field( $data, $key, $nested_key, $fallback = 'N/A' ) {
+    if ( isset( $data[$key] ) && is_array( $data[$key] ) && isset( $data[$key][$nested_key] ) && $data[$key][$nested_key] !== '' ) {
+        return $data[$key][$nested_key];
+    }
+
+    return $fallback;
+}
+
 function wporg_only_title_home_page( $title ) {
     global $wpdb;
-    
+
 	if ( get_query_var( 'pgpokeid' ) != '' ) {
-        
+
         $q = "SELECT * FROM ".$wpdb->prefix."ptp_cache_card WHERE permalink = '".get_query_var( 'pgpokename' )."' AND api_id = '".get_query_var( 'pgpokeid' )."'";
 	    $cache = $wpdb->get_row($q);
-        
-        $data = unserialize($cache->cached_meta);
-        
-		$title = $cache->name. ' • ' .$data['set']['name'] . ' • PrimetimePokemon.com';
-        
+
+        $data = ptp_priceguide_cached_card_data( $cache );
+
+        if ( $cache && $data ) {
+            $card_name = ! empty( $cache->name ) ? $cache->name : ptp_priceguide_scalar_field( $data, 'name', 'Price Guide' );
+            $set_name = ptp_priceguide_nested_scalar_field( $data, 'set', 'name', '' );
+
+            if ( ! empty( $set_name ) ) {
+                $title = $card_name. ' • ' .$set_name . ' • PrimetimePokemon.com';
+            } else {
+                $title = $card_name. ' • PrimetimePokemon.com';
+            }
+        }
+
 	}
 
 	return $title;
@@ -35,27 +74,33 @@ function filter_the_title_in_the_main_loop( $title ) {
         
         $q = "SELECT * FROM ".$wpdb->prefix."ptp_cache_card WHERE permalink = '".get_query_var( 'pgpokename' )."' AND api_id = '".get_query_var( 'pgpokeid' )."'";
 	    $cache = $wpdb->get_row($q);
-        
+
         if($cache) {
-        
-            $data = unserialize($cache->cached_meta);
-            
+
+            $data = ptp_priceguide_cached_card_data( $cache );
+            if ( ! $data ) {
+                return $title;
+            }
+
             $types = '';
-            if($data['subtypes']) {
-                $subtypes = $data['subtypes'];
+            $subtypes = ptp_priceguide_array_field( $data, 'subtypes' );
+            if($subtypes) {
                 $types = implode(", ",$subtypes);
             }
 
-            $title = $cache->name;
-            $title .= '<br/><small>'.$data['supertype'].'';
+            $supertype = ptp_priceguide_scalar_field( $data, 'supertype', '' );
+            $hp = ptp_priceguide_scalar_field( $data, 'hp', '' );
+
+            $title = ! empty( $cache->name ) ? $cache->name : ptp_priceguide_scalar_field( $data, 'name', $title );
+            $title .= '<br/><small>'.$supertype.'';
             if(!empty($types)) {
                 $title .= '- '.$types.'';
             }
             $title .= '</small>';
-            if($data['hp']) {
-                $title .= '<span>HP '.$data['hp'].'</span>';
+            if($hp) {
+                $title .= '<span>HP '.$hp.'</span>';
             }
-            
+
         }
         
     }
@@ -86,10 +131,27 @@ function filter_the_content_in_the_main_loop( $content ) {
         
         $q = "SELECT * FROM ".$wpdb->prefix."ptp_cache_card WHERE permalink = '".get_query_var( 'pgpokename' )."' AND api_id = '".get_query_var( 'pgpokeid' )."'";
 	    $cache = $wpdb->get_row($q);
-        
-        $data = unserialize($cache->cached_meta);
+
+        $data = ptp_priceguide_cached_card_data( $cache );
+        $data2 = '';
         //$data2 = '<pre>'.print_r($data,true).'</pre>';
-        
+
+        if ( ! $cache || ! $data ) {
+            $content = '<div class="pg-wrapper">';
+                $content .= '<div class="pg-section">';
+                    $content .= '<h2>Card Not Found</h2>';
+                    $content .= '<p>This Price Guide card could not be found. Please use the current card link from the Price Guide.</p>';
+                $content .= '</div>';
+            $content .= '</div>';
+
+            return $content;
+        }
+
+        $card_name = ! empty( $cache->name ) ? $cache->name : ptp_priceguide_scalar_field( $data, 'name', 'Unknown Card' );
+        $set_name = ptp_priceguide_nested_scalar_field( $data, 'set', 'name', 'Unknown Set' );
+        $printed_total = ptp_priceguide_nested_scalar_field( $data, 'set', 'printedTotal', 'N/A' );
+        $image_large = ! empty( $cache->image_large ) ? $cache->image_large : '';
+
         $content = '<div class="pg-wrapper">';
         
             $content .= '<script type="text/javascript">';
@@ -103,7 +165,11 @@ function filter_the_content_in_the_main_loop( $content ) {
         
             $content .= '<div class="third padding50r">';
 
-                $content .= '<a rel="sponsored" href="https://www.ebay.com/sch/i.html?_nkw='.$data['name'].'+'.$data['set']['name'].'&mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338833587&customid=&toolid=10001&mkevt=1" target="_blank"><img src="'.$cache->image_large.'" /></a>';
+                if ( ! empty( $image_large ) ) {
+                    $content .= '<a rel="sponsored" href="https://www.ebay.com/sch/i.html?_nkw='.$card_name.'+'.$set_name.'&mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338833587&customid=&toolid=10001&mkevt=1" target="_blank"><img src="'.$image_large.'" /></a>';
+                } else {
+                    $content .= '<p>Image unavailable.</p>';
+                }
 
             $content .= '</div>';
         
@@ -113,7 +179,7 @@ function filter_the_content_in_the_main_loop( $content ) {
         
                     $content .= '<h2 style="margin-top:0;">PRICES</h2>';
         
-                    $content .= '<h3 class="price-heading"><a rel="sponsored" href="https://www.ebay.com/sch/i.html?_nkw='.$data['name'].'+'.$data['set']['name'].'&mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338833587&customid=&toolid=10001&mkevt=1" id="btn" class="btn cardbtn" target="_blank">Get The Best Price on eBay</a></h3><h3 class="price-heading"><a href="https://www.tcgplayer.com/search/all/product?q='.$data['name'].'+'.$data['set']['name'].'&irclickid=ziS1UeXRBxyPUOxRQV25Yyc-UkHQ9hy3NxEyW80&sharedid=&irpid=5467273&irgwc=1&utm_source=impact&utm_medium=affiliate&utm_campaign=PrimetimePokemon.com" id="btn" class="btn cardbtn" target="_blank">Get The Best Price on TCGPlayer</a></h3><p class="callout">See the best prices and the latest price trends for ' .$data['name']. ' in the eBay or TCGPlayer marketplaces.</p>';
+                    $content .= '<h3 class="price-heading"><a rel="sponsored" href="https://www.ebay.com/sch/i.html?_nkw='.$card_name.'+'.$set_name.'&mkcid=1&mkrid=711-53200-19255-0&siteid=0&campid=5338833587&customid=&toolid=10001&mkevt=1" id="btn" class="btn cardbtn" target="_blank">Get The Best Price on eBay</a></h3><h3 class="price-heading"><a href="https://www.tcgplayer.com/search/all/product?q='.$card_name.'+'.$set_name.'&irclickid=ziS1UeXRBxyPUOxRQV25Yyc-UkHQ9hy3NxEyW80&sharedid=&irpid=5467273&irgwc=1&utm_source=impact&utm_medium=affiliate&utm_campaign=PrimetimePokemon.com" id="btn" class="btn cardbtn" target="_blank">Get The Best Price on TCGPlayer</a></h3><p class="callout">See the best prices and the latest price trends for ' .$card_name. ' in the eBay or TCGPlayer marketplaces.</p>';
         
                     $content .= '<div style="min-height:120px;" id="result_'.get_query_var( 'pgpokeid' ).'">';
         
@@ -233,7 +299,7 @@ function filter_the_content_in_the_main_loop( $content ) {
         
                     $content .= '<h2>ABILITIES</h2>';
         
-                    $abilities = $data['abilities'];
+                    $abilities = ptp_priceguide_array_field( $data, 'abilities' );
                     foreach($abilities as $a) {
                         
                         $content .= '<h3>'.$a['name'].'</h3>';
@@ -245,7 +311,7 @@ function filter_the_content_in_the_main_loop( $content ) {
                     $content .= '<div class="margin15"></div>';
         
                     $content .= '<h2>ATTACKS</h2>';
-                    $attacks = $data['attacks'];
+                    $attacks = ptp_priceguide_array_field( $data, 'attacks' );
                     foreach($attacks as $a) {
                         
                         $content .= '<h2>'.$a['name'].'<span><strong>Damage</strong>: '.$a['damage'].'</span></h2>';
@@ -260,7 +326,7 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>WEAKNESS</h3>';
         
-                        $weaknesses = $data['weaknesses'];
+                        $weaknesses = ptp_priceguide_array_field( $data, 'weaknesses' );
                         $xw = 0;
                         foreach($weaknesses as $w) {
                             
@@ -283,7 +349,7 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>RESISTANCE</h3>';
         
-                        $resistances = $data['resistances'];
+                        $resistances = ptp_priceguide_array_field( $data, 'resistances' );
                         $xw = 0;
                         foreach($resistances as $r) {
                             
@@ -306,11 +372,17 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>RETREAT COST</h3>';
         
-                        $retreatCost = $data['retreatCost'];
+                        $retreatCost = ptp_priceguide_array_field( $data, 'retreatCost' );
+                        $xr = 0;
                         foreach($retreatCost as $r) {
-                            
+
                             $content .= $r.' ';
-                            
+                            $xr++;
+
+                        }
+
+                        if($xr == 0) {
+                            $content .= 'N/A';
                         }
 
                     $content .= '</div>';
@@ -321,7 +393,7 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>ARTIST</h3>';
         
-                        $content .= $data['artist'];
+                        $content .= ptp_priceguide_scalar_field( $data, 'artist' );
 
                     $content .= '</div>';
         
@@ -329,7 +401,7 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>RARITY</h3>';
         
-                        $content .= $data['rarity'];
+                        $content .= ptp_priceguide_scalar_field( $data, 'rarity' );
 
                     $content .= '</div>';
         
@@ -337,7 +409,7 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>SET</h3>';
         
-                        $content .= $data['set']['name'];
+                        $content .= $set_name;
 
                     $content .= '</div>';
         
@@ -347,17 +419,17 @@ function filter_the_content_in_the_main_loop( $content ) {
 
                         $content .= '<h3>NUMBER</h3>';
         
-                        $content .= $data['number'].' / '.$data['set']['printedTotal'];
+                        $content .= ptp_priceguide_scalar_field( $data, 'number' ).' / '.$printed_total;
 
                     $content .= '</div>';
         
                     $content .= '<div class="clear"></div>';
         
-                    $content .= '<em>'.$data['flavorText'].'</em>';
+                    $content .= '<em>'.ptp_priceguide_scalar_field( $data, 'flavorText', '' ).'</em>';
         
                     $content .= '<div class="margin15"></div>';
 
-                    $legalities = $data['legalities'];
+                    $legalities = ptp_priceguide_array_field( $data, 'legalities' );
                     foreach($legalities as $key => $value) {
 
                         $content .= '<div class="third">';
@@ -381,7 +453,7 @@ function filter_the_content_in_the_main_loop( $content ) {
         
         $content .= '</div>';
         
-        $content .= do_shortcode('[primetime-related name="'.$cache->name.'"]');
+        $content .= do_shortcode('[primetime-related name="'.$card_name.'"]');
 
         $content .= $data2;
         
